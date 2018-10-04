14a41b17de196a931345aa57ec60262c5b684545
super_xin <m13423197429@163.com>
2018-10-04 23:26:00 +0800

    初步完成功能性增强 resolved #1

--- LoginUtil.go
+++ LoginUtil.go
@@ -1,28 +1,21 @@
-// LoginUtil.go
 package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
+
 	"github.com/axgle/mahonia"
 )
 
 //登陆
 var count = 0 //记录登陆次数
-
-func loginNet() (sign int, err error) {
+func loginNet(account, password string) (sign int, err error) {
 	const responseURL = "http://192.168.252.254"
-	var account, password string
-
-	fmt.Printf("Please input your accunt:")
-	fmt.Scanln(&account)
-	fmt.Printf("Please input your password:")
-	fmt.Scanln(&password)
-
 	var client *http.Client
 	client = http.DefaultClient
 
@@ -47,14 +40,8 @@ func loginNet() (sign int, err error) {
 
 	flag, _ := checkLogin()
 	if flag == false {
-		if count < 3 {
-			fmt.Println("login fail! Please check your account or password.")
-			loginNet()
-
-		} else {
-			fmt.Println("You have login four times!")
-			return 0, err
-		}
+		fmt.Println("login fail! Please check your account or password.")
+		return 0, err
 	}
 	fmt.Printf("The account of %s ", account)
 	return 1, err
@@ -80,6 +67,8 @@ func loginOut() {
 
 	return
 }
+
+//检查是否登陆
 func checkLogin() (flag bool, err error) {
 	flag = false
 	const URL = "http://192.168.252.254/"
@@ -95,7 +84,7 @@ func checkLogin() (flag bool, err error) {
 	body, err1 := ioutil.ReadAll(resp.Body)
 	if err1 != nil {
 		fmt.Println("error is ", err1)
-		return flag, err
+		return flag, err1
 	}
 	result := decoder.ConvertString(string(body))
 
@@ -104,13 +93,40 @@ func checkLogin() (flag bool, err error) {
 	return flag, nil
 
 }
-func main() {
+
+//强制切换用户
+func changeUser() (flag int, err error) {
+	var account, password string
+
+	fmt.Println("Please enter the new accout:")
+	fmt.Printf("Please input your accunt:")
+	fmt.Scanln(&account)
+	fmt.Printf("Please input your password:")
+	fmt.Scanln(&password)
+
+	flag, err = loginNet(account, password)
+	if err != nil {
+		fmt.Println("Login fail!")
+		return 0, err
+	}
+	if flag != 1 {
+		fmt.Println("Login fail!")
+		return 0, err
+	}
+	fmt.Println("change login success !")
+	return 1, nil
+}
+
+//初始化登录
+func loginInit() (r int, err1 error) {
+
 	var chose string
+	var account, password string
 	//检测账号是否已经登陆
 	flag, err := checkLogin()
 	if err != nil {
 		fmt.Println("error is ", err)
-		return
+		return 0, err
 	}
 	if flag == true {
 		fmt.Println("YOU ALREADY LOGIN!")
@@ -118,22 +134,121 @@ func main() {
 		fmt.Printf("Do you want to login out now?(y or n):")
 		fmt.Scanln(&chose)
 		if chose == "n" {
-			return
+			return 1, err
 		}
 		loginOut()
 	}
 	fmt.Printf("Do you want to login in now:(y or n)")
 	fmt.Scanln(&chose)
 	if chose == "n" {
-		return
+		return 1, nil
 	}
-	sign, err := loginNet()
+
+	fmt.Printf("Please input your accunt:")
+	fmt.Scanln(&account)
+	fmt.Printf("Please input your password:")
+	fmt.Scanln(&password)
+
+	sign, err := loginNet(account, password)
 	if err != nil {
 		fmt.Println("login error is ", err)
-		return
+		return 0, err
 	}
 	if sign == 1 {
 		fmt.Println("login success !")
 	}
+	return 1, nil
+
+}
+
+//下一步操作
+func nextAction() (sign int) {
+	var chose int
+	fmt.Println("Please enter next operate:")
+	fmt.Printf("1、change user  2、exit script:")
+	fmt.Scanln(&chose)
+	switch chose {
+	case 1:
+		flag, err := changeUser()
+		if err != nil {
+			fmt.Println("error is ", err)
+		}
+		if flag != 1 {
+			fmt.Println("change login fail!")
+		}
+	case 2:
+		return 1
+	default:
+		fmt.Println("Please enter the right chose!")
+	}
+	return 0
+}
+
+//命令行函数
+func command() (chose bool) {
+
+	pattern := flag.Bool("i", false, "Enter the interactive interface(true or false):")
+	loginleft := flag.Bool("l", false, "Disconnect the internet(true or false)")
+	account := flag.String("a", " ", "Input your accout")
+	password := flag.String("p", " ", "Input your password")
+	compulsive := flag.Bool("c", false, "Compulsive login")
+
+	flag.Parse()
+	sign, err := checkLogin()
+	if err != nil {
+		fmt.Print("check err have some errors")
+		return false
+	}
+
+	if *loginleft == true {
+		loginOut()
+		return false
+	}
+	if *pattern == true {
+		return *pattern
+	}
+	if *account != " " && *password != " " && (sign == false || *compulsive == true) {
+		sign, err := loginNet(*account, *password)
+		if err != nil {
+			fmt.Println("login error is ", err)
+			return
+		}
+		if sign == 1 {
+			fmt.Println("login success !")
+		}
+	} else if sign == true {
+		fmt.Println("You have Login,don't login once more!")
+	} else {
+		fmt.Println("optional arguments:")
+		fmt.Println("-i , Interactive interface ,   change into interactive interface(enter -i=true/false) ")
+		fmt.Println("-l , Loginout ,  Disconnect the internet(enter -l=true/false)")
+		fmt.Println("-a , Account ,  Input your login account")
+		fmt.Println("-p , Password , Input your login password")
+		fmt.Println("-c , Compulsive login , Compulsive login the Internet(enter -c=true/false)")
+
+	}
+	return false
+}
+func main() {
+	//默认为参数行执行
+	flag := command()
+	if flag == true {
+		fmt.Println("Welcome to the interactive interface:")
+		//执行初始化函数
+		result, err := loginInit()
+		if err != nil {
+			fmt.Println("error is ", err)
+			return
+		}
+		if result != 1 {
+			return
+		}
+		//执行下一步操作
+		sign := nextAction()
+		for sign == 0 {
+			sign = nextAction()
+		}
+	}
 	return
+
 }
